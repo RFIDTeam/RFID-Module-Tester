02cc90186fa3ef4205ae801719027eb4d62355d4
Allan <allan.lorant@insa-rennes.fr>
2013-01-21 15:20:01 +0100

    MRZ ajoutee par l'utilisateur

---
+++ src/callback.h
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <string.h>
+#include <gtk/gtk.h>
+
+void fct_ouvrir(GtkWidget *, gpointer);
+void fct_ouvrir2(GtkWidget *, gpointer);
+void lancer(GtkWidget *, gpointer);
---
+++ src/callback.h~
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+#include <gtk/gtk.h>
+
+void fct_ouvrir(GtkWidget *, gpointer);
+void fct_ouvrir2(GtkWidget *, gpointer);
+void lancer(GtkWidget *, gpointer);
